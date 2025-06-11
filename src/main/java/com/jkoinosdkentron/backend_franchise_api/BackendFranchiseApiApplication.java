72d7e3acb3a9d9e53e90ff026cdc3b106bb88f49
JkoinosDkentron <juandanielvargasatencio@gmail.com>
2025-06-11 09:52:24 -0500

    first commit

---
+++ src/main/java/com/jkoinosdkentron/backend_franchise_api/BackendFranchiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.jkoinosdkentron.backend_franchise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFranchiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFranchiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jkoinosdkentron/backend_franchise_api/BackendFranchiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkoinosdkentron.backend_franchise_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFranchiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
