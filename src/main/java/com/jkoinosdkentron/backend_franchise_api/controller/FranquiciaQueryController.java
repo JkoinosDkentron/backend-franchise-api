848aea4abf889df73da55a399a63b5937ccffce9
JkoinosDkentron <juandanielvargasatencio@gmail.com>
2025-06-12 08:54:41 -0500

    feat(controller): add query endpoint to retrieve most stocked products per sucursal

--- src/main/java/com/jkoinosdkentron/backend_franchise_api/controller/FranquiciaQueryController.java
+++ src/main/java/com/jkoinosdkentron/backend_franchise_api/controller/FranquiciaQueryController.java
@@ -1,4 +1,23 @@
 package com.jkoinosdkentron.backend_franchise_api.controller;
 
+import com.jkoinosdkentron.backend_franchise_api.application.query.GetTopStockedProductsQuery;
+import com.jkoinosdkentron.backend_franchise_api.application.handler.query.GetTopStockedProductsHandler;
+import com.jkoinosdkentron.backend_franchise_api.domain.model.Producto;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/franquicias")
 public class FranquiciaQueryController {
+
+    private final GetTopStockedProductsHandler topStockedProductsHandler;
+
+    public FranquiciaQueryController(GetTopStockedProductsHandler topStockedProductsHandler) {
+        this.topStockedProductsHandler = topStockedProductsHandler;
+    }
+
+    @GetMapping("/{id}/top-stock")
+    public Flux<Producto> getTopStockedProducts(@PathVariable String id) {
+        return topStockedProductsHandler.handle(new GetTopStockedProductsQuery(id));
+    }
 }
